3fe2be09392e6361d47a9480a909ed8ff7afe19e
Oliver Kiss <oliverkyss@gmail.com>
2021-09-25 13:27:26 +0200

    update

---
+++ _next/static/yr6kkVHDwujIHFOiQrCLK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/449cade73e38258748b0.css","static/chunks/pages/index-fb38080a2b0b8c507cba.js"],"/_error":["static/chunks/pages/_error-82a806cd39f8ab3dc3ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
